af2160b4fd22a7db579f3f9ff1c52aae8774b4aa
blurk <nsinh6745@gmail.com>
2024-12-19 00:01:51 +0700

    mouse move

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,8 @@
 use enigo::{
+    Axis::Vertical,
+    Coordinate::Rel,
     Direction::{Click, Press, Release},
-    Enigo, Key, Keyboard, Settings,
+    Enigo, Key, Keyboard, Mouse, Settings,
 };
 
 #[tauri::command]
@@ -51,6 +53,18 @@ fn trigger_key(key: &str) {
     }
 }
 
+#[tauri::command]
+fn move_mouse(x: i32, y: i32) {
+    let mut enigo = Enigo::new(&Settings::default()).unwrap();
+    enigo.move_mouse(x, y, Rel).unwrap();
+}
+
+#[tauri::command]
+fn scroll() {
+    let mut enigo = Enigo::new(&Settings::default()).unwrap();
+    enigo.scroll(1, Vertical).unwrap();
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
@@ -68,7 +82,7 @@ pub fn run() {
         })
         .plugin(tauri_plugin_positioner::init())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![trigger_key])
+        .invoke_handler(tauri::generate_handler![trigger_key, scroll, move_mouse])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/Macro.js
+++ src/Macro.js
@@ -4,6 +4,20 @@ async function sendKey(key = "") {
   await invoke("trigger_key", { key });
 }
 
+async function scroll() {
+  await invoke("scroll");
+}
+
+async function moveMouse(x = 20, y = 20) {
+  await invoke("move_mouse", { x, y });
+}
+
+function getRandomIntInclusive(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+
 class Macro {
   #stop = false;
   #pointer = 0;
@@ -54,6 +68,45 @@ class Macro {
     this.#pointer = 0;
   }
 
+  async callMoveMouse() {
+    const x = getRandomIntInclusive(-50, 50) || 50;
+    const y = getRandomIntInclusive(-50, 50) || 50;
+
+    let i = 0;
+    const step = Math.floor(Math.PI / Math.E);
+    const speed = 1000 / 60;
+
+    if (Math.random() > 0.5) {
+      if (x > 0) {
+        while (i < x) {
+          i += step;
+          await moveMouse(i, y);
+          await this.#wait(speed);
+        }
+      } else {
+        while (i > x) {
+          i -= step;
+          await moveMouse(i, y);
+          await this.#wait(speed);
+        }
+      }
+    } else {
+      if (y > 0) {
+        while (i < y) {
+          i += step;
+          await moveMouse(x, i);
+          await this.#wait(speed);
+        }
+      } else {
+        while (i > y) {
+          i -= step;
+          await moveMouse(x, i);
+          await this.#wait(speed);
+        }
+      }
+    }
+  }
+
   async run() {
     if (this.#stop) {
       return;
@@ -63,11 +116,14 @@ class Macro {
     let k = value;
 
     const seed = Math.random();
-    if (seed < 0.9) {
+    if (seed < 0.85) {
       this.#type(k);
       this.#nextChar();
       await this.#wait(this.delay * Math.random() + 321);
+    } else if (seed < 0.9) {
+      await this.callMoveMouse();
     } else if (seed < 0.95) {
+      await scroll();
     } else {
       await this.#wait(this.wait);
     }
